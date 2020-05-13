2995262737ef1926f1b7daf9b5d415c33064440b
chunsheng-chung-biz <chunsheng.chung@bizreach.co.jp>
2020-05-13 17:13:10 +0900

    Step1 complete

--- dbflute-twitterdb/dbflute_twitterdb/playsql/replace-schema.sql
+++ dbflute-twitterdb/dbflute_twitterdb/playsql/replace-schema.sql
@@ -2,14 +2,31 @@
 
 /* Create Tables */
 
--- this is sample table.
-CREATE TABLE SAMPLE
+-- User information
+CREATE TABLE USER
 (
-	SAMPLE_ID BIGINT NOT NULL AUTO_INCREMENT,
-	CREATED_AT DATETIME NOT NULL,
-	UPDATED_AT DATETIME NOT NULL,
-	PRIMARY KEY (SAMPLE_ID)
-) ENGINE = InnoDB COMMENT = 'サンプルのテーブル : this is sample table.';
+	NICKNAME VARCHAR(20) NOT NULL COMMENT 'Nickname : Displayed nickname for the account',
+	EMAIL VARCHAR(127) NOT NULL COMMENT 'Email address : Email address of the user. Also used as login information.',
+	USERNAME VARCHAR(20) NOT NULL COMMENT 'Username : Unique user id for @ mention etc.',
+	PASSWORD_HASH CHAR(32) NOT NULL COMMENT 'Hashed password',
+	PASSWORD_SALT CHAR(32) NOT NULL COMMENT 'Password salt : Random password salt for each user',
+	PHONE_NUMBER VARCHAR(20) COMMENT 'Phone number',
+	ADDRESS VARCHAR(255) COMMENT 'Address',
+	DATE_OF_BIRTH_AT DATE COMMENT 'Birthdate',
+	DISPLAY_LANGUAGE_ID INT NOT NULL COMMENT 'Display language ID',
+	COUNTRY_ID INT NOT NULL COMMENT 'Country ID',
+	JOIN_DATE_AT DATETIME NOT NULL COMMENT 'Join date',
+	PORTRAIT BLOB COMMENT 'Portrait',
+	BIO VARCHAR(1023) COMMENT 'biography',
+	LOCATION VARCHAR(127) COMMENT 'Location',
+	URL VARCHAR(255) COMMENT 'URL',
+	HEADER_IMAGE BLOB COMMENT 'Header image',
+	LAST_LOGIN_AT DATETIME COMMENT 'Last login',
+	UID BIGINT NOT NULL AUTO_INCREMENT COMMENT 'User ID : Unique UID in database as primary key.',
+	CREATED_AT DATETIME NOT NULL COMMENT 'Table creation date',
+	UPDATED_AT DATETIME NOT NULL COMMENT 'Table update date',
+	PRIMARY KEY (UID)
+) ENGINE = InnoDB COMMENT = 'User : User information';
 
 
 
